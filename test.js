4bbb58a40d58e2c1c08686ba7a25721e27e949b2
Stanley Haryoto <stanley.haryoto@gmail.com>
2022-01-14 11:36:42 +0800

    created makeAPICall function

---
+++ test.js
@@ -0,0 +1,14 @@
+// This is a simple example of making a PlayFab server API call
+handlers.makeAPICall = function (args, context) {
+    var request = {
+        PlayFabId: currentPlayerId, Statistics: [{
+                StatisticName: "Level",
+                Value: 2
+            }]
+    };
+    // The pre-defined "server" object has functions corresponding to each PlayFab server API 
+    // (https://api.playfab.com/Documentation/Server). It is automatically 
+    // authenticated as your title and handles all communication with 
+    // the PlayFab API, so you don't have to write extra code to issue HTTP requests. 
+    var playerStatResult = server.UpdatePlayerStatistics(request);
+};
